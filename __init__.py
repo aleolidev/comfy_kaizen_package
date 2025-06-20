40202303221e2136f4f2c4e52c97c161a9696a60
Alejandro Olivares <alejandroolivaresmompo@hotmail.com>
2025-06-21 00:17:16 +0200

    feat: :sparkles: init

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for kaizen_package."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Alejandro Olivares"""
+__email__ = "codekaizendev@gmail.com"
+__version__ = "0.0.1"
+
+from .src.kaizen_package.nodes import NODE_CLASS_MAPPINGS
+from .src.kaizen_package.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
